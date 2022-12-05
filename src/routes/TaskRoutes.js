c24a06e603aaa1c4d97b4a3c6eadee09a6ad1f62
edivaldo-acct <edivaldo.junior@acct.global>
2022-12-05 01:20:10 -0300

    :sparkles: find all
    Creating the fetch all method and creating the macaddress middleware to fetch all

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const TaskController = require('../controller/TaskController')
 const TaskValidation = require('../middlewares/TaskValidation')
+const MacaddressValidation = require('../middlewares/MacaddressValidation')
 
 router.post('/', TaskValidation, TaskController.create) 
 //creation of the routes, we put the middleare before the controller route so that validation is done before entering the route
-router.put('/:id', TaskController.update)
+router.put('/:id', TaskValidation, TaskController.update)
+router.get('/filter/all', MacaddressValidation, TaskController.all)
 
 module.exports = router
