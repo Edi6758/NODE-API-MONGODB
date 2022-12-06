ee77867c513801f0063d184669d26c4564e5eb90
Edi6758 <ediarts7@gmail.com>
2022-12-06 02:15:21 -0300

    :sparkles: Overdue Task Checker
    Overdue Task Checker

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,6 +1,8 @@
 const { response } = require('express')
 const TaskModel = require('../model/TaskModel')
 
+const current = new Date();
+
 class TaskController {
 
     async create(req, res) {
@@ -65,6 +67,20 @@ class TaskController {
             {'done': req.params.done},
             {new: true}
         )
+        .then(response => {
+            return res.status(200).json(response)
+        })
+        .catch(error => {
+            return res.status(500).json(error)
+        })
+    }
+
+    async late(req, res){
+        await TaskModel.find({
+            'when': {'$lt': current},
+            'macaddress': {'$in': req.body.macaddress}
+        }) //less than
+        .sort('when')
         .then(response => {
             return res.status(200).json(response)
         })
